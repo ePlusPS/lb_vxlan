cf99ff0dbcd1c478722f6f02043eb0a11b56d5b8
Robert Starmer <rstarmer@gmail.com>
2014-06-16 13:31:22 -0700

    Add "dmz" paramter for use in SJ13 DMZ

--- setup.sh
+++ setup.sh
@@ -255,6 +255,14 @@ if [ ! -d /root/puppet_openstack_builder ] ; then
   fi
 fi
 
+if [ ! -z "$dmz" ]; then
+sed -e 's/openstack-repo.cisco.com/10.0.100.21/' -i /root/puppet_openstack_repo/install-scripts/install.sh
+cat >> /root/puppet_openstack_builder/data/hiera_data/user.common.yaml <<EOF
+coe::base::openstack_repo_location: http://10.0.100.21/openstack/cisco
+coe::base::puppet_repo_location: http://10.0.100.21/openstack/puppet
+EOF
+fi
+
 # add ml2 parameters
 cat >> /root/puppet_openstack_builder/data/hiera_data/user.common.yaml <<EOF
 neutron::server::disableml2: false
