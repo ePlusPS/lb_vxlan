d4df3a450d453c429f7ec3797870aa6439f36380
Robert Starmer <rstarmer@gmail.com>
2014-06-22 10:59:55 -0700

    Update tests to use less common and more consistent addresses

--- tests/test.sh
+++ tests/test.sh
@@ -23,7 +23,7 @@ echo 8021q >> /etc/modules
 modprobe 8021q
 vconfig add eth0 144
 ip_host=`ip addr show eth0 | awk '/ inet / {print $2}' | cut -d/ -f1 | cut -d. -f4`
-ifconfig eth0.144 10.0.144.\$ip_host netmask 255.255.255.0 mtu 8950 up
+ifconfig eth0.144 	192.168.144.\$ip_host netmask 255.255.255.0 mtu 8950 up
 EOF
 fi
 
@@ -35,13 +35,13 @@ fi
 neutron net-create --tenant-id `keystone tenant-list | awk '/ openstack /  {print $2}'` \
   sharednet1 --shared --provider:network_type flat --provider:physical_network physnet1
 neutron subnet-create --ip-version 4 --tenant-id `keystone tenant-list | awk '/ openstack / {print $2}'` \
- sharednet1 10.0.149.0/24 --allocation-pool start=10.0.149.100,end=10.0.149.200 --dns_nameservers \
- list=true 10.0.100.15
+ sharednet1 192.168.149.0/24 --allocation-pool start=192.168.149.100,end=192.168.149.200 --dns_nameservers \
+ list=true 8.8.8.8
 
 neutron net-create --tenant-id `keystone tenant-list | awk '/ openstack /  {print $2}'` tenantnet1
 neutron subnet-create --ip-version 4 --tenant-id `keystone tenant-list | awk '/ openstack / {print $2}'` \
  tenantnet1 192.168.0.0/24 --allocation-pool start=192.168.0.100,end=192.168.0.200 --dns_nameservers \
- list=true 10.0.100.15
+ list=true 8.8.4.4
 
 nova boot --flavor 2 --image trusty --nic net-id=`neutron net-list | awk '/ sharednet1 /  {print $2}'` \
   --nic net-id=`neutron net-list | awk '/ tenantnet1 / {print $2}'` --key-name root \
