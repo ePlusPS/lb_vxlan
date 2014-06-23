6233241b8d06037ecae1936e85b2d28355531c1c
Robert Starmer <rstarmer@gmail.com>
2014-06-22 23:10:34 -0700

    fix puppet\openstack_builder puppet_openstack_builder

--- setup.sh
+++ setup.sh
@@ -413,9 +413,9 @@ neutron::agents::linuxbridge::physical_interface_mappings:\
 
 for n in `echo $interfaces | sed -e 's/:/ /g'` ; do
   iface_id=`echo $n | sed -e 's/\([0-9]*\)/\1/'`
-  sed -e "/flat_networks/a \ -\ physnet${iface_id}" -i /root/puppet/openstack_builder/install-scripts/install.sh
-  sed -e "/network_vlan_ranges/a \ -\ physnet${iface_id}" -i /root/puppet/openstack_builder/install-scripts/install.sh
-  sed -e "/physical_interface_mappings/a \ -\ physnet${iface_id}:${n}" -i /root/puppet/openstack_builder/install-scripts/install.sh
+  sed -e "/flat_networks/a \ -\ physnet${iface_id}" -i /root/puppet_openstack_builder/install-scripts/install.sh
+  sed -e "/network_vlan_ranges/a \ -\ physnet${iface_id}" -i /root/puppet_openstack_builder/install-scripts/install.sh
+  sed -e "/physical_interface_mappings/a \ -\ physnet${iface_id}:${n}" -i /root/puppet_openstack_builder/install-scripts/install.sh
 done
 
 echo "Setup Ml2_plugin network_plugin yaml"
